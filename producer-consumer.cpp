5b2c5a0cc10c034729ffef175bfdd75805c9c241
anna-chertova <ianet@yandex.ru>
2020-06-28 16:46:47 +0300

    Add containers to manage many producer/consumer threads

--- producer-consumer.cpp
+++ producer-consumer.cpp
@@ -4,7 +4,8 @@
  */
 
 #include <iostream>
-#include <queue>
+#include <vector>
+#include <memory>
 #include "pc_producer.h"
 #include "pc_consumer.h"
 #include "pc_shared_buffer.h"
@@ -24,25 +25,42 @@ int main()
     PCSharedBuffer buffer;
     buffer.init();
 
-    // Producer worker thread. Generates random integer numbers
-    PCProducer my_producer(buffer);
-    my_producer.init();
+    std::vector<std::unique_ptr<PCProducer>> producers;
+    std::vector<std::unique_ptr<PCConsumer>> consumers;
+
+    // Producer worker threads that generate random integer numbers.
+    for (int i = 0; i < NUM_PRODUCERS; i++) {
+        auto p(std::make_unique<PCProducer>(buffer));
+        p->init();
+        producers.push_back(std::move(p));
+    }
     
-    // Consumer worker thread. Reads integer numbers and prints them.
-    PCConsumer my_consumer(buffer);
-    my_consumer.init();
+    // Consumer worker threads. Read integer numbers and print them to console.
+    for (int i = 0; i < NUM_CONSUMERS; i++) {
+        auto c(std::make_unique<PCConsumer>(buffer));
+        c->init();        
+        consumers.push_back(std::move(c));
+    }
 
     // Main thread waits for user input
     char a;
     std::cin >> a;
 
     // Send producer and consumer threads stop command
-    my_producer.stop();
-    my_consumer.stop();
+    for (auto p = producers.begin(); p != producers.end(); ++p) {
+        p->get()->stop();
+    }
+    for (auto c = consumers.begin(); c != consumers.end(); ++c) {
+        c->get()->stop();
+    }
 
     // Wait threads to finalyze their work
-    my_producer.wait();
-    my_consumer.wait();
+    for (auto p = producers.begin(); p != producers.end(); ++p) {
+        p->get()->wait();
+    }
+    for (auto c = consumers.begin(); c != consumers.end(); ++c) {
+        c->get()->wait();
+    }
 
     return 0;
 }
