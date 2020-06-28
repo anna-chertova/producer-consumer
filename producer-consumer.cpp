2759a6c49aaf39580b1796807b1125e07b130d39
anna-chertova <ianet@yandex.ru>
2020-06-28 16:54:37 +0300

    Add re-starting program as many times as stated by NUM_REPEATS constant

--- producer-consumer.cpp
+++ producer-consumer.cpp
@@ -18,48 +18,53 @@ int main()
     setlocale(0, "");
 
     std::cout << "\nThis program demonstrates producer/consumer problem solution\n";
-    std::cout << "Enter any key to stop the program\n\n";
+    std::cout << "Enter any key to stop the program\n";
+    std::cout << "Number of program calls: " << NUM_REPEATS << "\n\n";
 
-    // This buffer contains "tasks".
-    // For now those are simple integer numbers.
-    PCSharedBuffer buffer;
-    buffer.init();
+    for (int n = 0; n < NUM_REPEATS; ++n) {
 
-    std::vector<std::unique_ptr<PCProducer>> producers;
-    std::vector<std::unique_ptr<PCConsumer>> consumers;
+        std::cout << "\n\nCall # " << (n + 1) << "/" << NUM_REPEATS << "\n\n";
+        // This buffer contains "tasks".
+        // For now those are simple integer numbers.
+        PCSharedBuffer buffer;
+        buffer.init();
 
-    // Producer worker threads that generate random integer numbers.
-    for (int i = 0; i < NUM_PRODUCERS; i++) {
-        auto p(std::make_unique<PCProducer>(buffer));
-        p->init();
-        producers.push_back(std::move(p));
-    }
-    
-    // Consumer worker threads. Read integer numbers and print them to console.
-    for (int i = 0; i < NUM_CONSUMERS; i++) {
-        auto c(std::make_unique<PCConsumer>(buffer));
-        c->init();        
-        consumers.push_back(std::move(c));
-    }
+        std::vector<std::unique_ptr<PCProducer>> producers;
+        std::vector<std::unique_ptr<PCConsumer>> consumers;
 
-    // Main thread waits for user input
-    char a;
-    std::cin >> a;
+        // Producer worker threads that generate random integer numbers.
+        for (int i = 0; i < NUM_PRODUCERS; i++) {
+            auto p(std::make_unique<PCProducer>(buffer));
+            p->init();
+            producers.push_back(std::move(p));
+        }
 
-    // Send producer and consumer threads stop command
-    for (auto p = producers.begin(); p != producers.end(); ++p) {
-        p->get()->stop();
-    }
-    for (auto c = consumers.begin(); c != consumers.end(); ++c) {
-        c->get()->stop();
-    }
+        // Consumer worker threads. Read integer numbers and print them to console.
+        for (int i = 0; i < NUM_CONSUMERS; i++) {
+            auto c(std::make_unique<PCConsumer>(buffer));
+            c->init();
+            consumers.push_back(std::move(c));
+        }
 
-    // Wait threads to finalyze their work
-    for (auto p = producers.begin(); p != producers.end(); ++p) {
-        p->get()->wait();
-    }
-    for (auto c = consumers.begin(); c != consumers.end(); ++c) {
-        c->get()->wait();
+        // Main thread waits for user input
+        char a;
+        std::cin >> a;
+
+        // Send producer and consumer threads stop command
+        for (auto p = producers.begin(); p != producers.end(); ++p) {
+            p->get()->stop();
+        }
+        for (auto c = consumers.begin(); c != consumers.end(); ++c) {
+            c->get()->stop();
+        }
+
+        // Wait threads to finalyze their work
+        for (auto p = producers.begin(); p != producers.end(); ++p) {
+            p->get()->wait();
+        }
+        for (auto c = consumers.begin(); c != consumers.end(); ++c) {
+            c->get()->wait();
+        }
     }
 
     return 0;
